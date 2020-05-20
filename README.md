6eed951d8c343970f26786744fb8faed624e4c12
EWebik <picsyprogramacion@gmail.com>
2020-05-19 19:01:39 -0500

    2. Introducción a JSX

--- README.md
+++ README.md
@@ -3,6 +3,19 @@ Espero que disfruten y aprovechen al máximo este tutorial de React JS, cada cap
 Este archivo será actualizado conforme vaya subiendo nuevos capítulos y cada actualización tendrá un link a la rama de cada capítulo, en cuestión y un link a mi página web donde se encontrarás toda la información del curso.
 Bien, espero les ayude mucho y me ayuden compartiendo en sus redes sociales.
 
-Capitulo 1 Cursro de React JS - Mi primera App con React
-Rama:master
+## Capitulo 1 Cursro de React JS - Mi primera App con React
+
+Aprenderás a levantar el ambiente de desarrollo de React en Ubuntu, podrás crear tu primera aplicación utilizando create-react-app, te mostraré la estructura de directorios y hacer tus primeros cambios.
+
+Rama: master
+
 Link detalles: https://ewebik.com/react-js
+
+## Capitulo 2 ¿Qué es React JSX? - Introducción a JSX
+
+Introducción a la sintaxis JSX, te enseñare a crear tus primeros elementos en React a través de React.createElement, aprenderás a migrar de React. createElement a la sintaxis JSX, conocerás el por qué los componentes deben empezar con mayúscula, comprenderás y aprenderás a utilizar propiedades en JSX en conjunto con el operador Spread.
+
+Rama: introduccion-jsx
+
+Link detalles: https://ewebik.com/react-js/jsx
+
--- src/index.js
+++ src/index.js
@@ -4,10 +4,67 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Creando un elemento con React.createElement
+
+//Elemento tipo parrafo 'p'
+var p = React.createElement('p',{className:'style-parrafo'},'Curso de React JS by EWebik');
+
+//Contenedor 'div'
+var container = React.createElement('div',{className:'container'},p);
+
+//Sintaxis JSX, reemplazando a React.createElement
+// class -> className
+
+var div = (
+    <div className='container'>
+      <p className='style-parrafo'>Curso de React JS by EWebik</p>
+    </div>
+);
+
+//Los componentes personalizados deben ser capitalizados
+function HolaEwebik(props){
+  return <p>Curso de React JS by EWebik</p>
+}
+
+function Contenedor(){
+  return <HolaEwebik />
+}
+
+//Manejar props o propiedades {}
+function HolaEwebikProps(props){
+  return (
+    <>
+      <p>{props.titulo + ' ' + '1'}</p>
+      <p>{props.titulo + ' ' + (1+props.indice)}</p>
+    </>
+    )
+}
+
+function Contenedor1(){
+  return <HolaEwebikProps titulo="Curso de React JS by EWebik - props" indice={10} />
+}
+
+//Perador Spread ...
+
+function Elemento1 (props){
+  return(
+    <div className='container'>
+      <p className='style-parrafo'>{"Titulo: " + props.titulo}</p>
+      <p className='style-parrafo'>{"Capitulo: " + props.capitulo}</p>
+    </div>
+  )
+}
+
+function Componente(){
+  const props = {
+    titulo:"Curso React JS EWebik",
+    capitulo:"Introducción a JSX"
+  }
+  return <Elemento1 {...props} />
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Componente />,
   document.getElementById('root')
 );
 
